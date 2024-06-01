3f006cc54ac8a132f6fb86af2c0a8e52e49b55fe
Lemon <minhlevu1642003@gmail.com>
2024-06-02 02:45:00 +0700

    write api for edit profile

--- src/main/java/org/petspa/petcaresystem/authenuser/controller/ProfileController.java
+++ src/main/java/org/petspa/petcaresystem/authenuser/controller/ProfileController.java
@@ -1,4 +1,22 @@
 package org.petspa.petcaresystem.authenuser.controller;
 
+import org.petspa.petcaresystem.authenuser.model.request.ProfileRequest.UpdateProfileRequest;
+import org.petspa.petcaresystem.authenuser.model.response.ResponseObj;
+import org.petspa.petcaresystem.authenuser.service.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/profile-management")
 public class ProfileController {
+    @Autowired
+    ProfileService profileService;
+
+    @PutMapping("/profile/{id}")
+    public ResponseEntity<ResponseObj> UpdateProflie(@PathVariable String id,
+                                                     @RequestBody UpdateProfileRequest profileRequest){
+        return profileService.UpdateProflie(id, profileRequest);
+    }
+
 }
