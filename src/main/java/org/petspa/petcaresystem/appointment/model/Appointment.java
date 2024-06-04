31fc1538d00eaf7f4d31a7c744e7cf3da0290dc3
Lemon <minhlevu1642003@gmail.com>
2024-06-04 12:58:36 +0700

    fix conlict

--- src/main/java/org/petspa/petcaresystem/appointment/model/Appointment.java
+++ src/main/java/org/petspa/petcaresystem/appointment/model/Appointment.java
@@ -9,14 +9,9 @@ import org.petspa.petcaresystem.enums.Status;
 import org.petspa.petcaresystem.pet.model.entity.Pet;
 import org.petspa.petcaresystem.service_and_combo.model.Services;
 
-<<<<<<< HEAD
 
 import java.time.LocalDateTime;
 import java.util.Date;
-=======
-import java.util.HashSet;
-import java.util.Set;
->>>>>>> 9d2f3f6e7400c6e1b7ced54009e8623c2153260e
 
 @Entity
 @Getter
@@ -38,7 +33,6 @@ public class Appointment {
     @Column(name = "status")
     private Status status;
 
-<<<<<<< HEAD
     @Column(name = "create_date")
     private Date create_date;
 
@@ -55,22 +49,4 @@ public class Appointment {
     @ManyToOne
     @JsonIgnore
     private Services Combo_id;
-=======
-    @Column(name = "createdDate")
-    private int createdDate;
-
-    @Column(name = "appointmentTime")
-    private int appointmentTime;
-
-    @OneToOne(mappedBy = "serviceId")
-    private Services service;
-
-//     @ManyToMany
-//     @JoinTable(
-//             name = "Appointment_Combo",
-//             joinColumns = @JoinColumn(name = "appointment_id"),
-//             inverseJoinColumns = @JoinColumn(name = "Combo_id")
-//     )
-//     private Set<Combo> combos = new HashSet<>();
->>>>>>> 9d2f3f6e7400c6e1b7ced54009e8623c2153260e
 }
