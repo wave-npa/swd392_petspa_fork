cf3ea6d83af0aeb306020bbb33ade9bfb7470ae9
Wave <ngnphucan@gmail.com>
2024-07-15 12:44:28 +0700

    fix review entity

--- src/main/java/org/petspa/petcaresystem/appointment/model/response/AppointmentResponseData.java
+++ src/main/java/org/petspa/petcaresystem/appointment/model/response/AppointmentResponseData.java
@@ -9,7 +9,7 @@ import org.petspa.petcaresystem.doctor.model.DoctorData;
 import org.petspa.petcaresystem.enums.Status;
 import org.petspa.petcaresystem.order.model.UserOrder;
 import org.petspa.petcaresystem.pet.model.entity.Pet;
-import org.petspa.petcaresystem.review.model.Review;
+import org.petspa.petcaresystem.review.model.entity.Review;
 import org.petspa.petcaresystem.serviceAppointment.model.ServicesData;
 
 import java.time.LocalDate;
--- src/main/java/org/petspa/petcaresystem/review/model/entity/Review.java
+++ src/main/java/org/petspa/petcaresystem/review/model/entity/Review.java
@@ -3,6 +3,7 @@ package org.petspa.petcaresystem.review.model.entity;
 import jakarta.persistence.*;
 import org.petspa.petcaresystem.appointment.model.payload.Appointment;
 import org.petspa.petcaresystem.authenuser.model.payload.AuthenUser;
+import org.petspa.petcaresystem.enums.ReviewRating;
 import org.petspa.petcaresystem.enums.Status;
 
 import lombok.AllArgsConstructor;
@@ -30,7 +31,8 @@ public class Review implements Serializable {
     private String description;
 
     @Column(name = "rating")
-    private Float rating;
+    @Enumerated(EnumType.STRING)
+    private ReviewRating rating;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "status")
