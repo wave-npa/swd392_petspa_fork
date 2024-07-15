f3b2add14f4925aebbd5c38a2771748703239d79
nlhchinh <chinhnguyen1692000z@gmail.com>
2024-07-15 14:34:28 +0700

    Fix deployment issue

--- src/main/java/org/petspa/petcaresystem/config/SecurityConfig.java
+++ src/main/java/org/petspa/petcaresystem/config/SecurityConfig.java
@@ -121,7 +121,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173") // front end port
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
